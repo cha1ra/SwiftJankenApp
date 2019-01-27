04b3e5d4378cb93792f818203a3ae4f53707c0d5
cha1ra <cha1ra.garnet@gmail.com>
2019-01-28 02:34:53 +0900

    Initial Commit

---
+++ JankenProduct2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JankenProduct2
+//
+//  Created by 坂尻愛明 on 2019/01/28.
+//  Copyright © 2019 cha1ra.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
